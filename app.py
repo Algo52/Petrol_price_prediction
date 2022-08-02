54d5b5cd5f26bd33c32cc9ff4ab3ab2781318f79
Algo52 <hemanthhemu5999@gmail.com>
2022-08-02 20:23:57 +0530

    created logging and exception class

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from crypt import methods
-from distutils.log import debug
-from tkinter import PROJECTING
+import logging,sys
 from flask import Flask
+from Petrol_price.logger import logging
+from Petrol_price.exception import ForcastException
 
 
 app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
+    try:
+        raise Exception("djhfgdsfgdfkdsfkjds")
+    except Exception as e:
+        forcast=ForcastException(e,sys)
+        logging.info(forcast.error_message)
+        logging.info("we are pasting logginf")
     return "starting mahine learning PROJECTING and completed ci/cd pipeline"
 
 
