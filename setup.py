e4f7910e6dd0d57f9e589da9b769d0d7f9773596
Algo52 <hemanthhemu5999@gmail.com>
2022-08-02 18:47:37 +0530

    created the pakages

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 
@@ -10,16 +10,15 @@ def get_requirements_list()->List[str]:
     of libraries mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readline()
+        return requirement_file.readline().remove("-e .") 
 
 
 # Declaring variables for setup function
 PROJECT_NAME = 'Petrol_price_prediction'
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 AUTHOR = 'Hemanth'
 DESCRIPTION = "ML Project Deployment Process"
 REQUIREMENT_FILE_NAME = 'requirements.txt'
-PACKAGES = ["Petrol_price"]
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 
@@ -29,7 +28,7 @@ setup(
     version = VERSION,
     author = AUTHOR,
     description = DESCRIPTION,
-    packages = PACKAGES,
+    packages = find_packages(),
     install_requires = get_requirements_list()
 )
 
