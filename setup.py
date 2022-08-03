0531602316b08eeccfc62d3828c3f1455b35e844
Algo52 <hemanthhemu5999@gmail.com>
2022-08-03 13:18:32 +0530

    notes

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_requirements_list()->List[str]:
     of libraries mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readline().remove("-e .") 
+        return requirement_file.readline()
 
 
 # Declaring variables for setup function
